f6577a49107e8c0ef5677a4a3a75037a106505e0
Sergey <sergeypronyuk@gmail.com>
2020-05-19 15:37:59 +0300

    refactor axios methods

--- utilities/helper/axiosRequestHelper.js
+++ utilities/helper/axiosRequestHelper.js
@@ -2,29 +2,9 @@ const { axiosRequest } = require('utilities/request');
 
 const apiServer = process.env.API_SERVER || 'http://localhost:3001';
 
-exports.connectBattle = async ({ call }) => {
-  if (call.params.cryptoName && call.params.playerID && call.params.battleID) {
-    const body = {
-      cryptoName: call.params.cryptoName,
-      playerID: call.params.playerID,
-      battleID: call.params.battleID,
-    };
-    return axiosRequest({ url: `${apiServer}/api/connect-battle`, params: body, viewRequest: 'post' });
-  }
-  return { error: 'error params' };
-};
+exports.connectBattle = async ({ call }) => axiosRequest({ url: `${apiServer}/api/connect-battle`, params: call.params, viewRequest: 'post' });
 
-exports.createBattle = async ({ call }) => {
-  if (call.params.cryptoName && call.params.playerID && call.params.healthPoints) {
-    const body = {
-      cryptoName: call.params.cryptoName,
-      playerID: call.params.playerID,
-      healthPoints: call.params.healthPoints,
-    };
-    return axiosRequest({ url: `${apiServer}/api/create-battle`, params: body, viewRequest: 'post' });
-  }
-  return { error: 'error params' };
-};
+exports.createBattle = async ({ call }) => axiosRequest({ url: `${apiServer}/api/create-battle`, params: call.params, viewRequest: 'post' });
 
 exports.getBattlesByState = async () => axiosRequest({ url: `${apiServer}/api/show-battles-by-state/select?state=start`, viewRequest: 'get' });
 
