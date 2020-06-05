c5e7eda6e8241058ae7bcd841520568488701c82
Boris Andrzhievskyi <borees2@gmail.com>
2020-06-05 15:45:03 +0300

    change validation

--- utilities/validators/battleValidator.js
+++ utilities/validators/battleValidator.js
@@ -9,8 +9,10 @@ exports.createBattleSchema = Joi.object().keys({
       .valid('BAT', 'BCH', 'BTC', 'DASH', 'EOS', 'ETH', 'LTC', 'NEO', 'WAVES', 'XMR', 'XRP', 'ZEC', 'ZRX')
       .required(),
     playerID: Joi.string().required(),
-    healthPoints: Joi.number().required(),
-  }),
+    healthPoints: Joi.number().min(1).required(),
+    betType: Joi.string().valid('HIVE', 'HBD', 'STEEM', 'SBD'),
+    amount: Joi.number().min(1),
+  }).with('betType', 'amount').with('amount', 'betType'),
 }).options(options);
 
 exports.connectBattleShcema = Joi.object().keys({
@@ -21,5 +23,7 @@ exports.connectBattleShcema = Joi.object().keys({
       .required(),
     playerID: Joi.string().required(),
     battleID: Joi.string().required(),
-  }),
+    betType: Joi.string().valid('HIVE', 'HBD', 'STEEM', 'SBD'),
+    amount: Joi.number().min(1),
+  }).with('betType', 'amount').with('amount', 'betType'),
 }).options(options);
